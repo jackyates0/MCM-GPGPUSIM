8fe3ab0774affa7a6fe21e4a48e05e72b0dfb11b
Jack <jackjackattack3056@protonmail.com>
2025-04-08 18:25:22 +0000

    Fixed wrong name commiting

--- src/gpgpu-sim/l2cache.cc
+++ src/gpgpu-sim/l2cache.cc
@@ -263,6 +263,7 @@ void memory_partition_unit::simple_dram_model_cycle() {
         }
         m_dram_latency_queue.pop_front();
       }
+      
     } else {
       this->set_done(mf_return);
       delete mf_return;
