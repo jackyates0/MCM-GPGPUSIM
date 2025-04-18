b368dbff1ce4595f39f7e4ebc13e020014d2935d
Jack <jackjackattack3056@protonmail.com>
2025-04-18 16:59:43 +0000

    im dumb

--- src/gpgpu-sim/gpu-sim.cc
+++ src/gpgpu-sim/gpu-sim.cc
@@ -233,7 +233,7 @@ void power_config::reg_options(class OptionParser *opp) {
 }
 
 void memory_config::reg_options(class OptionParser *opp) {
-  option_parser_register(opp, "gpgpu_n_chiplets", OPT_UINT32, &n_chiplets,
+  option_parser_register(opp, "-gpgpu_n_chiplets", OPT_UINT32, &n_chiplets,
                          "Number of MCM chiplets", "1");
   option_parser_register(opp, "-gpgpu_perf_sim_memcpy", OPT_BOOL,
                          &m_perf_sim_memcpy, "Fill the L2 cache on memcpy",
